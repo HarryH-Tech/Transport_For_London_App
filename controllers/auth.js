e525a3a1eb790b68fe1edd9f6c811804f86b27e6
HarryH-Tech <h.harrison1993@gmail.com>
2022-01-22 15:21:06 +0300

    Specified JWT algorithm (RS256)

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
-const User = require("../models/user");
-const jwt = require("jsonwebtoken"); //generates signed token
-const expressJwt = require("express-jwt"); //for authrozation check
-const { errorHandler } = require("../helpers/dbErrorHandler");
+const User = require('../models/user');
+const jwt = require('jsonwebtoken'); //generates signed token
+const expressJwt = require('express-jwt'); //for authrozation check
+const { errorHandler } = require('../helpers/dbErrorHandler');
 
 exports.signUp = (req, res) => {
   const user = new User(req.body);
@@ -25,14 +25,14 @@ exports.signIn = (req, res) => {
   User.findOne({ email }, (err, user) => {
     if (err || !user) {
       return res.status(400).json({
-        error: "User with that email does not exist, please sign up.",
+        error: 'User with that email does not exist, please sign up.',
       });
     }
     //If user found ensure username and pw match
     //create authenticate method in user model
     if (!user.authenticate(password)) {
       return res.status(401).json({
-        error: "Email and Password do not match",
+        error: 'Email and Password do not match',
       });
     }
 
@@ -40,7 +40,7 @@ exports.signIn = (req, res) => {
     const token = jwt.sign({ _id: user.id }, process.env.JWT_SECRET);
 
     //persist token as "t" in cookie with expiry date
-    res.cookie("t", token, { expire: new Date() + 9999 });
+    res.cookie('t', token, { expire: new Date() + 9999 });
 
     //return res with user + token to frontend
     const { _id, username, email, role } = user;
@@ -50,22 +50,23 @@ exports.signIn = (req, res) => {
 
 exports.signOut = (req, res) => {
   //clear cookie
-  res.clearCookie("t");
+  res.clearCookie('t');
   res.json({
-    message: "Sign Out Success",
+    message: 'Sign Out Success',
   });
 };
 
 exports.requireSignIn = expressJwt({
   secret: process.env.JWT_SECRET,
-  userProperty: "auth",
+  userProperty: 'auth',
+  algorithms: ['RS256'],
 });
 
 exports.isAuth = (req, res, next) => {
   let user = req.profile && req.auth && req.profile_id == req.auth._id;
   if (!user) {
     return res.status(403).json({
-      error: "Access Denied",
+      error: 'Access Denied',
     });
   }
   next();
@@ -74,7 +75,7 @@ exports.isAuth = (req, res, next) => {
 exports.isAdmin = (req, res, next) => {
   if (req.profile.role === 0) {
     return res.status(403).json({
-      error: "Access Denied",
+      error: 'Access Denied',
     });
   }
   next();
